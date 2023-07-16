06630d186c51b0e2b74c1842457239e086ce55a0
RubenTriana <rubendariotriana@gmail.com>
2023-07-15 19:39:13 -0500

    nav bar y accordion fixed

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import './accordion.css';
 
-const accordion = () => {
+const AccordionComponent = () => {
     return (
-
         <div name='services' className="container-fluid">
             <div className="row background-section">
                 <div className='col-1'></div>
                 <div className="col-md-6 col-sm-12 d-flex align-items-center">
                     <Accordion className="w-100" defaultActiveKey="0" flush>
-                        <Accordion.Item ClassName="" eventKey="0">
+                        <Accordion.Item className="" eventKey="0">
                             <Accordion.Header>Sé audaz y destaca</Accordion.Header>
                             <Accordion.Body>
                             ¿Estás listo para marcar tendencia y destacar entre la multitud? En NEVA, te ofrecemos las gorras más modernas y vanguardistas del mercado. Nuestras gorras personalizadas son el accesorio perfecto para expresar tu estilo único y atrevido. Con una amplia gama de colores y diseños exclusivos, te aseguramos que encontrarás la gorra perfecta para complementar tu look y resaltar tu personalidad. Únete a la comunidad de NEVA y déjate llevar por la moda y la innovación. ¡No esperes más para lucir audaz y marcar la diferencia con NEVA!
@@ -29,7 +28,6 @@ const accordion = () => {
             </div>
         </div>
     );
-
 }
 
-export default accordion;
+export default AccordionComponent;
